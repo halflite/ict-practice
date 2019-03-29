11e7ceeabaaf8c71e4b51e6822ba5283c580a57f
Shingo Hayashibara <shingo@halflite.net>
2019-03-30 00:01:02 +0900

    Spring Boot初期コミット

---
+++ app/src/test/java/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author shingo
+ *
+ */
+module app {
+	exports com.example.todo.app;
+
+	requires junit;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.boot.test;
+	requires spring.test;
+}
