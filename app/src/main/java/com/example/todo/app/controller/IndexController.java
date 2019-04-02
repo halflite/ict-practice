46a0c94dfdf5ad87b4649cad3b08eeabdbf91fbe
Shingo Hayashibara <shingo@halflite.net>
2019-04-03 03:17:24 +0900

    入力フォームに文字の長さ制限をつける

--- app/src/main/java/com/example/todo/app/controller/IndexController.java
+++ app/src/main/java/com/example/todo/app/controller/IndexController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.NoSuchMessageException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
